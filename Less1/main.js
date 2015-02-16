8f3218c0d3ad877d391c29a5f6685bf87114bc2e
Anastasiia Ostapova <anastasia.ostapka@gmail.com>
2015-02-16 18:09:44 +0200

    hometask

---
+++ Less1/main.js
@@ -0,0 +1,15 @@
+var genre = "fantasy";
+
+if (genre == "drama") {
+	console.log("The american tradegy");
+} else if (genre == "fantasy") {
+	console.log("The Hobbit");
+} else if (genre == "detective") {
+	console.log("The last secret");
+} else if (genre == "science fatasy") {
+	console.log("I, robot");
+} else if (genre == "philosophy") {
+	console.log("Thus Spoke Zarathustra");
+} else {
+	console.log("ololo");
+}
