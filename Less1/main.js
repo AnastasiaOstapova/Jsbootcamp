3daf0861f463df4c02610d4b1186628a28c76356
Anastasiia Ostapova <anastasia.ostapka@gmail.com>
2015-02-17 14:19:15 +0200

    added first task

--- Less1/main.js
+++ Less1/main.js
@@ -1,15 +1,27 @@
-var genre = "fantasy";
+var bookTitle = "The American Tradegy";
+var authorName = "Theodore Dreiser";
+var numberOfPages = 500;
+var isFiction = false;
+var publishingYear = 1925;
 
-if (genre == "drama") {
-	console.log("The american tradegy");
-} else if (genre == "fantasy") {
-	console.log("The Hobbit");
-} else if (genre == "detective") {
-	console.log("The last secret");
-} else if (genre == "science fatasy") {
-	console.log("I, robot");
-} else if (genre == "philosophy") {
-	console.log("Thus Spoke Zarathustra");
+
+
+console.log("Type of book title is " + typeof bookTitle);
+console.log("Type of author name is " + typeof authorNam);
+console.log("Type of number of pages is " + typeof numberOfPages);
+console.log("Type of variable isFiction is " + typeof isFiction);
+console.log("Type of publishing year is " + typeof publishingYear);
+
+console.log("--------------------------------------------");
+
+var genre = "ololo";
+
+if (genre == "fiction") {
+	console.log("I Robot");
+} else if (genre == "novel") {
+	console.log("Pride and Prejudice");
+} else if (genre == "biography") {
+	console.log("The Twelve Caesars");
 } else {
-	console.log("ololo");
+	console.log("Introduction to Quantum Mechanics");
 }
