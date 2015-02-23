a60ef124ca11004dd4708f5382c61123c6c23880
Anastasiia Ostapova <anastasia.ostapka@gmail.com>
2015-02-23 13:20:11 +0200

    added condition to if statement to verify end of the string

--- Hometask2/main.js
+++ Hometask2/main.js
@@ -1,4 +1,5 @@
-var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae fringilla ligula, mattis bibendum arcu. Curabitur finibus eleifend scelerisque. Aliquam convallis turpis sit amet augue tincidunt, ac ultricies magna porttitor. Vivamus posuere non leo vitae pretium. Pellentesque facilisis pellentesque molestie. Phasellus consequat euismod odio vel tincidunt. Vivamus laoreet purus quis molestie facilisis. Donec et sollicitudin diam, suscipit dignissim tortor. Aliquam faucibus interdum tortor quis lacinia. Pellentesque a consectetur urna. Proin at elit elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula tristique ligula mollis dapibus. Aenean in felis justo. Nullam eget elit in quam blandit finibus. Lorem ipsum.";
+// var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae fringilla ligula, mattis bibendum arcu. Curabitur finibus eleifend scelerisque. Aliquam convallis turpis sit amet augue tincidunt, ac ultricies magna porttitor. Vivamus posuere non leo vitae pretium. Pellentesque facilisis pellentesque molestie. Phasellus consequat euismod odio vel tincidunt. Vivamus laoreet purus quis molestie facilisis. Donec et sollicitudin diam, suscipit dignissim tortor. Aliquam faucibus interdum tortor quis lacinia. Pellentesque a consectetur urna. Proin at elit elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula tristique ligula mollis dapibus. Aenean in felis justo. Nullam eget elit in quam blandit finibus. Lorem ipsum.";
+ var text = "A quick black fox jumped over the lazy dog";
 
 var counter = 0;
 var notAlphabet = " ,.!?";
@@ -10,7 +11,7 @@ var wordsMoreThan6SymbolsLength = 0;
 
 while (counter < text.length) {
 	var currentWord = "";
-	while (notAlphabet.indexOf(text[counter]) == -1) {
+	while (notAlphabet.indexOf(text[counter]) == -1 && counter != text.length - 1) {
 		currentWord += text[counter];
 		counter++;
 	}
@@ -25,5 +26,5 @@ while (counter < text.length) {
 }
 
 console.log("Words with length less than 4 letters: " + wordsLessThan4SymbolsLength);
-console.log("Words with length between 4 and 6 4 letters: " + wordsBetween4And6SymbolsLength);
+console.log("Words with length between 4 and 6 letters: " + wordsBetween4And6SymbolsLength);
 console.log("Words with length more than 6 letters: " + wordsMoreThan6SymbolsLength);
